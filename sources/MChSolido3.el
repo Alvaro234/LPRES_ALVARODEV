7caccd17f9883ed97686e77ddeb4a2a91d0e3eda
Alvaro <a.gonzalezv@alumnos.upm.es>
2022-06-20 12:05:40 +0200

    Experimento de validacion preparado, correciones menores en comb erosiva
    Pendiente de corregir velocidad de recesion en gasto por combustion en volumenes
    
    Signed-off-by: Alvaro <a.gonzalezv@alumnos.upm.es>

--- sources/MChSolido3.el
+++ sources/MChSolido3.el
@@ -7,26 +7,33 @@ USE LPRES
 USE MATH
 
 PORT SRB2 SINGLE
-EQUAL							 REAL Coord				   UNITS u_m				RANGE ZERO,Inf			"Coordinate"	
-EQUAL                    REAL g                 UNITS u_kg_s         RANGE ZERO,Inf       "Mass flow"
-EQUAL							 REAL Pt				     UNITS u_Pa				RANGE ZERO,Inf		   "Total temperature"
+EQUAL							 REAL Coord				  UNITS u_m				RANGE ZERO,Inf			"Coordinate"	
+EQUAL                    REAL g                UNITS u_kg_s       RANGE ZERO,Inf       "Mass flow"
+EQUAL							 REAL Pt				     UNITS u_Pa			RANGE ZERO,Inf		   "Total temperature"
 EQUAL							 REAL Tt				     UNITS u_K				RANGE ZERO,Inf		   "Total temperature"
 
+----- Poner que se pasen las propiedades del gas, gamma y R ??
 
-CONTINUOUS
+
 END PORT
+
 -------------------------------------------------------------------------------------------------------------------------------------------------------
+
 COMPONENT Pared2
 	PORTS
 		OUT SRB2 salida
 	DATA
-		REAL Tc = 3330
+		REAL Tc = 3330	 UNITS u_K	RANGE ZERO,Inf "Adiabatic combustion temperature"
 	CONTINUOUS 
-	salida.Tt = Tc		
-	salida.g = 0
-	salida.Coord = 0
+		-------------- Head End Boundary Conditions -----------------
+		salida.Tt = Tc				
+		salida.g = 0
+		salida.Coord = 0
+		-------------------------------------------------------------
 END COMPONENT
+
 -----------------------------------------------------------------------------------------------------------------------------------------------------
+
 COMPONENT Conversor2
 	PORTS
 		IN SRB2 entrada
@@ -37,16 +44,47 @@ COMPONENT Conversor2
 		REAL gamma = 1.33
 		--- Geometria tobera ---
 		REAL D0 = 0.0254
+		
+		REAL ER = 10
+		REAL Pa = 101325
+		---- Erosion garganta ---
+		REAL alfa = 0.0002		"Velocidad de recesion de la garganta"
+	DECLS
+		------------- 
+		REAL rth			"Recesion de la tobera"
 		REAL A_th = 8.00E-03
+		------------- Actuacion tobera ---------------
+		DISCR REAL Fgamma			
+		REAL Ct
+		REAL T
+		REAL Pexit
+		REAL TotalImpulse
+		REAL ISP
+	INIT
+		Fgamma = sqrt(gamma)* ( 2/(gamma+1) )**( (gamma+1)/(2*(gamma-1)) )
+		TotalImpulse = 0
+		rth = D0/2
+		A_th = MATH.PI*(D0/2)**2
 	CONTINUOUS		
-		1 = ((entrada.g*c_star)/(A_th*entrada.Pt))  -- Condicion de igualdad de gastos entre combustion y tobera 
-		 ------ Erosion cte de la garganta -----
-		 /*
+	
+		c_star = (A_th*entrada.Pt)/entrada.g -- Condicion de igualdad de gastos entre combustion y tobera --
+		 
+		------ Erosion lineal de la garganta -----
+		 
 		 rth' = alfa			-- Valor inicial rth = D0/2
-		 At=MATH.PI*(rth)**2
-		 */	
-		------------- ECUACIONES TOBERA  ----------------
+		 A_th=MATH.PI*(rth)**2
+		 
+		 	
+		------------- ECUACIONES TOBERA (Adaptada) ----------------
 			
+		--ER = Fgamma/ (( (Pexit/entrada.Pt)**(1/gamma)) * sqrt((   2*gamma/(gamma-1) )*  (1- (Pexit/entrada.Pt)**((gamma-1)/gamma) )  ))    
+		Pexit = Pa
+		Ct = Fgamma*sqrt((   2*gamma/(gamma-1) )*  (1- (Pexit/entrada.Pt)**((gamma-1)/gamma) )       )
+		T = Ct*entrada.Pt*A_th
+		TotalImpulse' = T
+		ISP = TotalImpulse/(entrada.g*9.81)
+		
+	
 END COMPONENT
 -------------------------------------------------------------------------------------------------------------------------------------------------------
 COMPONENT MChSolido3(INTEGER N = 3, BOOLEAN Comb_Erosiva = TRUE )
@@ -54,26 +92,24 @@ COMPONENT MChSolido3(INTEGER N = 3, BOOLEAN Comb_Erosiva = TRUE )
 		IN  SRB2 entrada
 		OUT SRB2 salida
 	DATA
-			
 			-- DATOS GENERALES COMBUSTIBLE Y PARAMETROS COMBUSTION
-			REAL gamma = 1.33
-			REAL Rho_P = 1750							 "Densidad del propelente"
-			REAL M_m = 26.144    UNITS u_g 	 "Masa molar del propelente"
-			REAL R_gas = 307.93						 "Constante R del gas"
-			REAL Tc = 3300 							 "Temperatura de combustion adiabatica"
-			REAL Cp = 1005
+			REAL gamma = 1.33		UNITS no_units	 "Relacion de calores especificos"
+			REAL Rho_P = 1750		UNITS u_kg_m3	 "Densidad del propelente"
+			REAL M_m = 26.144    UNITS u_g 	 	 "Masa molar del propelente"
+			REAL R_gas = 307.93	UNITS u_J_kgK	 "Constante R del gas"
+			REAL Tc = 3300 		UNITS u_K		 "Temperatura de combustion adiabatica"
 			-- Ley de vieille  
-			REAL Exponente =	0.4					 "Exponente de Vieille"
-			REAL Factor_a =	1.27955E-05			 "Factor de Vieille"	
+			REAL Exponente =	0.4					 "Vieille law pressure exponent"
+			REAL Factor_a =	1.27955E-05			 "Vieille law coefficient"	
 			-- Combustion erosiva
 			REAL Viscosidad_camara =	1.80E-05	 "Viscosidad de camara"
 		   REAL gth =	35								 "Parametro de erosion"
 			-- DATOS GEOMETRIA
-			TABLE_1D S_y_1 = { {0,  0.2,0.200001}, -- Y
-							  {0.075,0.2,0} } "Tabla S(y) de la interfaz de entrada" -- S 
+			TABLE_1D S_y_1 = { {0, 2}, -- Y
+							  		 {1, 2} } "Tabla S(y) de la interfaz de entrada" -- S 
 		
-			TABLE_1D S_y_N = { {0, 0.004,0.0040001,0.2}, -- Y
-							  {0.185,0.185,0,0} }  "Tabla S(y) de la interfaz de salida"-- S	
+			TABLE_1D S_y_N = { {0, 2}, -- Y
+							  		 {1, 2} }  "Tabla S(y) de la interfaz de salida"-- S	
 							  
 			REAL L = 0. UNITS u_m 		  "Longitud de la seccion"
 			REAL D = 0.0635 UNITS u_m	  "Diametro carcasa cilindrica"
@@ -120,11 +156,12 @@ COMPONENT MChSolido3(INTEGER N = 3, BOOLEAN Comb_Erosiva = TRUE )
 					REAL eta[N]
 								
 	INIT		-----------Configuracion geometrica inicial, esto es provisional, se tendria que introducir en el experimento?-----------
-		dx = L/N	
+		dx = L/N	   -- Malla equiespaciada --- 
 		FOR(i IN 1,N+1)
 			Y[i] = 0
-			--S[i] = 2*MATH.PI*0.5*D0				// De momento los valores iniciales se suponen los de un cilindro
-		   Ap[i] = MATH.PI*(0.5*D0)**2
+			
+		   Ap[i] = MATH.PI*(0.5*D0)**2 // De momento los valores de areas iniciales se suponen los de un cilindro
+			
 			A_carcasa = MATH.PI*(D/2)**2
 			Combustion[i] = 1
 		-- Aqui habria que implementar una distribucion de presiones inicial a partir de P0 = (rho_p*a*c_estrella* Abtotal/At)**(1/(1-n)) Quizas con una 
@@ -132,11 +169,11 @@ COMPONENT MChSolido3(INTEGER N = 3, BOOLEAN Comb_Erosiva = TRUE )
 		END FOR
 	DISCRETE
 		EXPAND(i IN 1,N+1)
-			WHEN (Y[i] >= (D-D0)/2) TOL 1E-05 THEN
+			WHEN (Y[i] >= (D-D0)/2) TOL 1E-03 THEN
 				Combustion[i] = 0			// La combustion en una interfaz acaba cuando se llega a la carcasa
 			END WHEN
-			WHEN(SUM(i IN 1,N+1; Combustion[i]) <= N*0.6 ) THEN
-		      STOP "******************NO FUEL LEFT*********************" // Realmente es que queden 3 interfaces o menos por quemar
+			WHEN(SUM(i IN 1,N+1; Combustion[i]) <= N*0.6 ) THEN			-- Cuando la derivada de la masa sea negativa?
+		      --STOP "******************NO FUEL LEFT*********************" // Realmente es que queden 3 interfaces o menos por quemar
 				// Se podria hacer cuando la masa de propelente sea un 1-5% de la inicial (reg stacionario)?
 			END WHEN
 	CONTINUOUS		
@@ -145,15 +182,11 @@ COMPONENT MChSolido3(INTEGER N = 3, BOOLEAN Comb_Erosiva = TRUE )
 		Coord[N+1]= salida.Coord
 		g[1] = entrada.g
 		g[N+1] = salida.g
-		--U[1] = entrada.U
-		--U[N+1] = salida.U
-		
 		Pt[1]= entrada.Pt
 		Pt[N]= salida.Pt
 		Tt[1] = entrada.Tt
 		Tt[N]= salida.Tt 
 		----------------------------------------------------------------INTERFACES (De 1 a N+1)---------------------------------------------------------------------
-		
 		EXPAND_BLOCK(i IN 1,N)
 			Coord[i+1] = Coord[i] + dx			 
 			g[i+1] = g[i]+dg[i]
@@ -164,7 +197,7 @@ COMPONENT MChSolido3(INTEGER N = 3, BOOLEAN Comb_Erosiva = TRUE )
 		(g[i+1]+0.5*dg[i+1])*Uc[i+1] + Apc[i+1]*P[i+1] = (g[i]+0.5*dg[i])*Uc[i]+Apc[i]*P[i]-- Ec cant mov
 		END EXPAND_BLOCK
 		------------------ Geometria -------------------
-			Y[1]'	 = (2*r0[1]-Y[2]')*Combustion[1]				--- Modificar mediante una aproximacion mejor
+			Y[1]'	 = (2*r0[1]-Y[2]')*Combustion[1]		-- Los extremos se aproximan mediante la velocidad de recesion del volumen y la interfaz anterior
 			Y[N+1]'= (2*r0[N]-Y[N]')*Combustion[N+1]
 		EXPAND_BLOCK(i IN 2,N)
 			Y[i]' = 	0.5*(r0[i]+r0[i-1])*Combustion[i]
@@ -177,13 +210,11 @@ COMPONENT MChSolido3(INTEGER N = 3, BOOLEAN Comb_Erosiva = TRUE )
 		EXPAND_BLOCK(i IN 1,N+1) 
 			Ap[i]' = S[i] * Y[i]'
 		END EXPAND_BLOCK
-
 ---------------------------------------------------------------------- VOLUMEN DISCRETO (De 1 a N)--------------------------------------------------------------
-			
 		EXPAND_BLOCK (i IN 1,N)  -- 1 a N 
 			Ab[i] = IF((Combustion[i]+Combustion[i+1])==0) 0 ELSE 0.5*(S[i]+S[i+1])*dx
 			
-			dg[i] = Rho_P*rp0[i]*Ab[i] 			-- Cambiar rp0 por r0 
+			dg[i] = Rho_P*rp0[i]*Ab[i] 					-- Aqui rp0 tendria que ser r0 para que el gasto sea correcto... 
 			rp0[i] = (Factor_a*P[i]**Exponente)
 			r0[i] = rp0[i]*eta[i]    
 			
@@ -214,7 +245,7 @@ COMPONENT MChSolido3(INTEGER N = 3, BOOLEAN Comb_Erosiva = TRUE )
 			EXPAND_BLOCK(i IN 1,N)  -- 1 a N
 			g0_i[i] = (Rho[i]*Uc[i])/(Rho_P*rp0[i])  -- Cuidado con las velocidades aqui
 			g_i[i] = g0_i[i]*(Re0[i]/1000)**-0.125
-			Re0[i] = (Rho_P*rp0[i]*(S[i]/MATH.PI))/Viscosidad_camara  -- El Dh esta modificado segun paper para evitar sobrepresiones
+			Re0[i] = (Rho_P*rp0[i]*(0.5*(S[i]+S[i+1])/MATH.PI))/Viscosidad_camara  -- El Dh esta modificado segun paper para evitar sobrepresiones
 			eta_temp[i] =	1 + 0.023*(g_i[i]**0.8-gth**0.8) 
 			eta[i] = IF(eta_temp[i]<1) 1 ELSE eta_temp[i]
 			END EXPAND_BLOCK 
